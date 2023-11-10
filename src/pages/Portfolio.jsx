cc689faff38d9ad3907465bad0d28ba7c22cdb62
Sarah Wesley <srwesley722@gmail.com>
2023-11-10 15:15:34 -0600

    Added content to the Portfolio and Resume .jsx Page files and updated the package.json file

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,9 @@
+import ProjectList from "../components/UI/Card/CardBody";
+
+export default function PortfolioPage() {
+    return (
+        <>
+            <ProjectList />
+        </>
+    );
+}
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,5 @@
+import Resume from "../components/UI/Resume";
+
+export default function ResumePage() {
+    return <Resume />
+}
