f53667948d1761ac137e0adddc35c5d1423a723b
Sarah Wesley <srwesley722@gmail.com>
2023-11-10 09:25:56 -0600

    Added the utils folder and sub files

---
+++ src/utils/helpers.js
@@ -0,0 +1,5 @@
+// Regex for validating an email
+export function validateEmail(email) {
+    const emailVal = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailVal.test(String(email).toLowerCase());
+}
